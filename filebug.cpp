7c9856672e67228b702aa8b14f17401dd238a9b6
Vegard Stenhjem Hagen <vegard.hagen@ntnu.no>
2018-04-17 16:47:32 +0200

    minimal working example of reading non-existent file

---
+++ filebug.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+int main () {
+    int n = 10;
+    float *buf = new float[n];
+
+    std::string fname = "test.bin";
+    std::ifstream file;
+
+    std::remove(fname.c_str());
+
+    // Opening a non-existent file
+    file.open(fname, std::ios::in | std::ios::binary);
+
+    size_t pos = sizeof(float)*0;
+    file.seekg(pos, std::ios::beg);
+    file.read(reinterpret_cast<char*>(buf),sizeof(float)*n);
+
+    for (int i=0; i<n; i++) {
+        std::cout << buf[i] << std::endl;
+    }
+
+    return 1;
+}
+
