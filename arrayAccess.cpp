9b2a60e1b766b87a55460369c4b3ff585fcfffe7
Vegard Stenhjem Hagen <vegard.hagen@ntnu.no>
2018-05-15 14:40:00 +0200

    Pointers to pointers testing.

--- arrayAccess.cpp
+++ arrayAccess.cpp
@@ -11,7 +11,6 @@ int main() {
     int nx = 5;
     int ny = 5;
     int ntot = nx*ny;
-
     float* M = new float[ntot];
 
     for (int i=0; i<ntot; i++) {
@@ -23,8 +22,41 @@ int main() {
             std::cout <<  M[i+nx*j] << " ";
         }
     }
-
     std::cout << std::endl;
 
-    return 0;
+    std::cout << "Incrementation: " << std::endl;
+    int p = 0;
+    int q = 0;
+    for (int i=0; i<5; i++) {
+        std::cout << "++p " << ++p << std::endl;
+        std::cout << "q++ " << q++ << std::endl;
+    }
+
+    std::cout << "p   " << p << std::endl;
+    std::cout << "q   " << q << std::endl;
+
+    int b[6] = {0,0,0,0,0,0};
+
+    std::cout << b[2] << std::endl;
+
+    std::cout << "Pointers to pointers: " << std::endl;
+    float *k = new float[4];
+    float *l = new float[4];
+    float *m = new float[4];
+
+    for (int i=0; i<4; i++) {
+        k[i] = 1;
+        l[i] = 2;
+        m[i] = 3;
+    }
+
+    float **H = new float*[3]{k,l,m};
+    float *n = H[0];
+
+    for (int i=0; i<4; i++) {
+        std::cout << "n[i]:    " << n[i] << std::endl;
+        std::cout << "H[1][i]: " << H[1][i] << std::endl;
+    }
+
+    return EXIT_SUCCESS;
 }
